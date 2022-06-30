37d48fccc241372675c7c55d716fb4fda54faed8
John-MacAulay <88356693+John-MacAulay@users.noreply.github.com>
2022-06-30 15:07:40 +1000

    checking flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
-import InputField  from './Components/InputField';
-
-
 import './App.css';
+import InputField from './Components/InputField';
+import { toDoItem} from './model';
 
 const App: React.FC = () => {
-  
-const [toDoItem, setToDoItem] = useState<string>("")
-const [toDoItemList, setToDoItemList] = useState([])
+
+  const [toDo, setToDoItem] = useState<string>("");
+  const [toDoItemList, setToDoItemList] = useState<toDoItem[]>([]);
+
+  const handleAddItemOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();  // this bit stops the default refresh on submitting
+
+    if (toDo) {
+      setToDoItemList([...toDoItemList, { id: Date.now(), toDoText: toDo , isDone: false }]);
+
+      setToDoItem("");
+    }
+  }
+
+  console.log(toDoItemList);
 
   return (
     <div className="App">
-    <span className= "heading"> This is the Header</span>
-      <InputField toDoItem={toDoItem} setToDoItem = {setToDoItem}/>
+      <span className="heading"> This is the Header</span>
+      <InputField toDo={toDo} setToDoItem={setToDoItem} handleAdd={handleAddItemOnSubmit} />
     </div>
   );
 }
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './styles.css';
 
 interface inputFieldPropsTypes{
-  toDoItem: string;
+  toDo: string;
   setToDoItem: React.Dispatch<React.SetStateAction<string>>;
-
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-export const InputField = ({toDoItem, setToDoItem} :inputFieldPropsTypes) => {
+export const InputField = ({toDo, setToDoItem, handleAdd} :inputFieldPropsTypes) => {
   return (
-    <form className='input'>
+    <form className='input' onSubmit={handleAdd} >
       <input type='input' placeholder='Enter your Task' className='input__box' />
       <button className='input_submit' type='submit'>
         Submit
